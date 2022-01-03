e2944f74af261d2c00126161ccef857b2e1c35e8
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-01-03 19:46:32 -0300

    Add files via upload

---
+++ Mundo.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class Mundo 
+{
+
+    public static void main(String[] args) 
+    {
+        int op;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Digite 1 para exibir a mensagem na língua portuguesa.");
+        System.out.println("Digite 2 para exibir a mensagem na língua espanhola.");
+        System.out.println("Digite 3 para exibir a mensagem na língua francesa.");
+        System.out.println("Digite 4 para exibir a mensagem na língua italiana.");
+        System.out.println("Digite 5 para exibir a mensagem na língua inglesa.");
+        System.out.println("Digite 6 para exibir a mensagem na língua alemã.");
+        System.out.println("Digite 7 para exibir a mensagem na língua dinamarquesa.");
+        System.out.println("Digite 8 para exibir a mensagem na língua holandesa.");
+        System.out.println("Digite 9 para exibir a mensagem na língua norueguesa.");
+        System.out.println("Digite 10 para exibir a mensagem na língua sueca.");
+        System.out.println("Digite 11 para exibir a mensagem na língua finlandesa.");
+        System.out.println("Digite 12 para exibir a mensagem na língua islandesa.");
+        System.out.println("Digite 13 para exibir a mensagem na língua grega.");
+        System.out.println("Digite 14 para exibir a mensagem na língua latina.");
+        System.out.println("Digite 15 para exibir a mensagem na língua romena.");
+        System.out.println("Digite 16 para exibir a mensagem na língua polonesa.");
+        System.out.println("Digite 17 para exibir a mensagem na língua eslovaca.");
+        System.out.println("Digite 18 para exibir a mensagem na língua sérvia.");
+        System.out.println("Digite 19 para exibir a mensagem na língua eslovena.");
+        System.out.println("Digite 20 para exibir a mensagem na língua croata.");
+        System.out.println("Digite 21 para exibir a mensagem na língua húngara.");
+        System.out.println("Digite 22 para exibir a mensagem na língua turca.");
+        System.out.println("Digite 23 para exibir a mensagem na língua ucraniana.");
+        System.out.println("Digite 24 para exibir a mensagem na língua russa.");
+        System.out.println("Digite 25 para exibir a mensagem na língua hebraica.");
+        System.out.println("Digite 26 para exibir a mensagem na língua árabe.");
+        System.out.println("Digite 27 para exibir a mensagem na língua chinesa.");
+        System.out.println("Digite 28 para exibir a mensagem na língua coreana.");
+        System.out.println("Digite 29 para exibir a mensagem na língua japonesa.");
+        System.out.println("Digite 30 para exibir a mensagem na língua hindi.");
+
+        System.out.println("");
+        System.out.println("");
+        
+        op = ler.nextInt();
+        if(op==1)
+            System.out.println("Olá Mundo.");
+        else if(op==2)
+            System.out.println("Hola mundo.");
+        else if(op==3)
+            System.out.println("Bonjour le monde.");
+        else if(op==4)
+            System.out.println("Ciao mondo.");
+        else if(op==5)
+            System.out.println("Hello World.");
+        else if(op==6)
+            System.out.println("Hallo Welt.");
+        else if(op==7)
+            System.out.println("Hej Verden.");
+        else if(op==8)
+            System.out.println("Hallo Wereld.");
+        else if(op==9)
+            System.out.println("Hei Verden.");
+        else if(op==10)
+            System.out.println("Hej världen.");
+        else if(op==11)
+            System.out.println("Hei maailma.");
+        else if(op==12)
+            System.out.println("Halló heimur.");
+        else if(op==13)
+            System.out.println("Γειά σου Κόσμε (Geiá sou Kósme).");
+        else if(op==14)
+            System.out.println("Salve Mundo.");
+        else if(op==15)
+            System.out.println("Salut Lume.");
+        else if(op==16)
+            System.out.println("Witaj świecie.");
+        else if(op==17)
+            System.out.println("Ahoj svet.");
+        else if(op==18)
+            System.out.println("Здраво Свете (Zdravo Svete).");
+        else if(op==19)
+            System.out.println("Pozdravljen svet.");
+        else if(op==20)
+            System.out.println("Pozdrav svijete");
+        else if(op==21)
+            System.out.println("Helló Világ");
+        else if(op==22)
+            System.out.println("Selam Dünya");
+        else if(op==23)
+            System.out.println("Привіт Світ (Pryvit Svit)");
+        else if(op==24)
+            System.out.println("Привет мир (Privet mir)");
+        else if(op==25)
+            System.out.println("(shlom lech oalm) שלום עולם");
+        else if(op==26)
+            System.out.println("(marhaban bialealam) مرحبا بالعالم");
+        else if(op==27)
+            System.out.println("(Nǐ hǎo shìjiè) 你好 世界");
+        else if(op==28)
+            System.out.println("(hellowoldeu) 헬로월드");
+        else if(op==29)
+            System.out.println("(Konnichiwa sekai) こんにちは世界");
+        else if(op==30)
+            System.out.println("नमस्ते दुनिया (namaste duniya)");
+    }
+    
+}
